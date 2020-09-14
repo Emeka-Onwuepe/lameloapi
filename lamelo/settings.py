258e4bd59801f75f3a8766e2b404344b23168eae
casdon_mystery <pascalemy2010@gmail.com>
2020-09-14 23:45:05 +0100

    worked on Cors

--- lamelo/settings.py
+++ lamelo/settings.py
@@ -64,7 +64,8 @@ REST_KNOX = {
 }
 
 
-CORS_ORIGIN_WHITELIST = ["http://127.0.0.1:3000", "http://localhost:3000", "*"]
+CORS_ORIGIN_WHITELIST = ["http://127.0.0.1:3000",
+                         "http://localhost:3000", "http://*"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
