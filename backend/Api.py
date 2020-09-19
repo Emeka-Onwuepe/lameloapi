87c41b081494bd730e73bb76aa36eeb42a5e0e84
casdon_mystery <pascalemy2010@gmail.com>
2020-09-19 16:44:23 +0100

    feat: some adjustsments

--- backend/Api.py
+++ backend/Api.py
@@ -16,36 +16,31 @@ class GetProducts(generics.GenericAPIView):
             returned = []
             for item in wants:
                 cat = Category.objects.get(name=item)
-                # size = Size.objects.filter(multiplesizes__category__id=cat.id)
-                # pureList = list(dict.fromkeys(size))
-                # prices = SizeSerializer(pureList, many=True)
+                size = Size.objects.filter(multiplesizes__category__id=cat.id)
+                pureList = list(dict.fromkeys(size))
+                prices = SizeSerializer(pureList, many=True)
                 products = ProductSerializer(cat.products, many=True)
-                # , "prices": prices.data
-                for product in products.data:
-                    returned.append(product)
-                # returned.append(products.data)
-
+                returned.append({item: products.data, "prices": prices.data})
             return Response(returned)
         else:
             cat = Category.objects.get(name=action)
             product = cat.products
-            # size = Size.objects.filter(multiplesizes__category__id=cat.id)
-            # pureList = list(dict.fromkeys(size))
-            # prices = SizeSerializer(pureList, many=True)
+            size = Size.objects.filter(multiplesizes__category__id=cat.id)
+            pureList = list(dict.fromkeys(size))
+            prices = SizeSerializer(pureList, many=True)
             products = ProductSerializer(product, many=True)
-            # , "prices": prices.data
-            return Response({action: products.data})
+            return Response({action: products.data, "prices": prices.data})
 
 
-class GetProduct(generics.GenericAPIView):
-    serializer_class = ProductSerializer
+# class GetProduct(generics.GenericAPIView):
+#     serializer_class = ProductSerializer
 
-    def post(self, request, *args, **kwargs):
-        product = Product.objects.get(id=int(request.data["id"]))
-        size = Size.objects.filter(multiplesizes__id=product.id)
-        cat = Category.objects.get(products=product)
-        pro = Product.objects.filter(catproduct=cat.id).exclude(id=product.id)
-        related = ProductSerializer(pro, many=True)
-        prices = SizeSerializer(size, many=True)
-        products = ProductSerializer(product)
-        return Response({"product": products.data, "prices": prices.data, "related": related.data})
+#     def post(self, request, *args, **kwargs):
+#         product = Product.objects.get(id=int(request.data["id"]))
+#         size = Size.objects.filter(multiplesizes__id=product.id)
+#         cat = Category.objects.get(products=product)
+#         pro = Product.objects.filter(products=cat.id).exclude(id=product.id)
+#         related = ProductSerializer(pro, many=True)
+#         prices = SizeSerializer(size, many=True)
+#         products = ProductSerializer(product)
+#         return Response({"product": products.data, "prices": prices.data, "related": related.data})
--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
-from .Api import GetProducts, GetProduct
+from .Api import GetProducts
+# , GetProduct
 from django.urls import path
 router = routers.DefaultRouter()
 
 urlpatterns = [
     path('getproducts', GetProducts.as_view(), name="getproducts"),
-    path('getproduct', GetProduct.as_view(), name="getproduct"),
+    # path('getproduct', GetProduct.as_view(), name="getproduct"),
 ]
 
 urlpatterns += router.urls
