5c3c9dea557a56911108aced787551f5a5b12989
casdon_mystery <pascalemy2010@gmail.com>
2020-10-12 16:32:56 +0100

    debuging

--- backend/Api.py
+++ backend/Api.py
@@ -131,7 +131,7 @@ class DashBoardView(generics.GenericAPIView):
     serializer_class = OrderedSerializer
 
     def get(self, request, *args, **kwargs):
-        ordered = Ordered.objects.filter(paid=True).filter(archived=False)
+        ordered = Ordered.objects.filter(paid=True)
         orderdSerializer = OrderedSerializer(ordered, many=True)
         return Response({"ordered": orderdSerializer.data})
 
